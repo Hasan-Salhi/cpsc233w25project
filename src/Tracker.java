50a4a31a754b4d40095b279e7c17c916a86844f7
jade.torres <jade.torres@ucalgary.ca>
2025-02-25 11:49:31 -0700

    Initial commit

---
+++ src/Tracker.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ * CPSC 233 W25 Project Tracker Class
+ * A class for tracking Pokemon statistics.
+ *
+ * @author Jade Torres, Jordan Tran, Hasan Salhi T01
+ * @version 1.0 - February 25, 2025
+ */
+public class Tracker {
+    public static void addTeam() {
+        //TODO
+    }
+
+    public static void addPokemon(String name, int hp, String typeOne, String typeTwo) {
+        //TODO
+    }
+
+    public static void addMove(String move) {
+        //TODO
+    }
+
+    public static void addItem(String item) {
+        //TODO
+    }
+
+    public static void addWin() {
+        //TODO
+    }
+
+    public static void addLoss() {
+        //TODO
+    }
+
+    public static void getAllPokemon() {
+        //TODO
+    }
+
+    public static void getTopAttack () {
+        //TODO
+    }
+
+    public static void getTopHP () {
+        //TODO
+    }
+
+    public static void getAverageAttack () {
+        //TODO
+    }
+
+    public static void getAllType (String type) {
+        //TODO
+    }
+
+    public static void main(String[] args) {
+        //TODO
+    }
+}
